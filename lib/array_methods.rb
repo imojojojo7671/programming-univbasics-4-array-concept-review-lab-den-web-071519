661bc257064bb202c2598106132cf77d81106df4
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-06 17:28:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,10 @@ def find_element_index(array, value_to_find)
   if array[index] == value_to_find
     return index
   end
+  puts
   }
 end
-# Add your solution here
+
 
 
 def find_max_value(array)
